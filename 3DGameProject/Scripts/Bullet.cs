0fe7b6c9ff456b9897fb0c40789090347aa9eb92
elimike10 <150400832+elimike10@users.noreply.github.com>
2025-03-02 19:34:17 -0800

    Add 3DGameProject/Scripts/Bullet.cs 1740972854899

---
+++ 3DGameProject/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
